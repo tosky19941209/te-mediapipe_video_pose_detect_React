660495c189acdf548c38daa32b4b5dc440690e7c
felixeliass1994 <felixeliass1994@gmail.com>
2024-02-17 03:03:05 +1000

    add_com

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
-
+import Camera from './page/camera'
+import Result from './page/result'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Camera></Camera>
+        <Result></Result>
       </header>
     </div>
   );

a2951c0b6ed1daf599c7e777b3fae9df3ee2240e
felixeliass1994 <felixeliass1994@gmail.com>
2024-02-17 04:07:05 +1000

    as

--- src/page/camera.js
+++ src/page/camera.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 function Camera () {
+    
+    
     return (
         <>
         </>
--- src/page/result.js
+++ src/page/result.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Result () {
+    const[couter, setcounter] = useState(null)
     return (
         <>
         </>
